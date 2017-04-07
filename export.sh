52f64066dcd90d443d467b41f81a78202c7d7044
patomdev <patryk.omiotek@gmail.com>
2017-04-07 12:39:12 +0200

    fixed export file and removed hardcoded local ip - its helpful when using docker machine

--- export.sh
+++ export.sh
@@ -2,4 +2,5 @@
 NOW=$(date +"%m_%d_%Y")
 FILE="wp-data/data_$NOW.sql"
 docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $FILE
-sed -i 1,1d $FILE # Removes the password warning from the file
+NEW_CONTENT=$(sed -n '1!p' $FILE) # Removes the password warning from the file
+echo $NEW_CONTENT > $FILE
