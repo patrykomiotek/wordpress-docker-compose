faa65705b5060a6bdb104cdda4bdad31665fe5ca
patomdev <patryk.omiotek@gmail.com>
2017-04-07 11:29:36 +0200

    fixed export file

--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
-_now=$(date +"%m_%d_%Y")
-_file="wp-data/data_$_now.sql"
-docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $_file
-sed -i 1,1d $_file # Removes the password warning from the file
+#!/bin/bash
+NOW=$(date +"%m_%d_%Y")
+FILE="wp-data/data_$NOW.sql"
+docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $FILE
+sed -i 1,1d $FILE # Removes the password warning from the file
